022fd4072e2c7eae3c32ac77b6cec469f4fb66d3
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-03-15 22:36:49 -0300

    criando o corpo da função

--- main.dart
+++ main.dart
@@ -1,3 +1,20 @@
-void main(List<String> args){
+import 'dart:io';
+
+void main(List<String> args) {}
+
+int menu() {
+  var op;
+
+  print('========== Menu ==========');
+
+  print('[1] para somar');
+  print('[2] para subtrair');
+  print('[3] para multiplicar');
+  print('[4] para dividir');
+  print('[5] para sair');
+  op = stdin.readLineSync();
+
   
+
+  return 10;
 }
